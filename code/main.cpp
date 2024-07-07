8c5e35e9178780192c275201cca27c7d7e3aead8
oh-facts <as17793@nyu.edu>
2024-07-07 10:35:26 +0530

    even more shit

--- code/main.cpp
+++ code/main.cpp
@@ -20,9 +20,62 @@
 #define submit_clear_screen() write(STDOUT_FILENO,"\x1b[2J", 4)
 #define submit_reset_cursor() write(STDOUT_FILENO, "\x1b[H", 3)
 
+enum MK_KEY
+{
+	MK_KEY_NONE,
+	
+	MK_KEY_UP,
+	MK_KEY_DOWN,
+	MK_KEY_RIGHT,
+	MK_KEY_LEFT,
+	
+	MK_KEY_DEL,
+	
+	
+	MK_KEY_COUNT
+};
+
+MK_KEY mk_key_from_char(char c)
+{
+	MK_KEY out = {};
+	if(c == '\x1b')
+	{
+		char seq[3];
+		read(STDIN_FILENO, &seq, 3);
+		
+		switch(seq[1])
+		{
+			case 'A':
+			{
+				out = MK_KEY_UP;
+			}break;
+			case 'B':
+			{
+				out = MK_KEY_DOWN;
+			}break;
+			case 'C':
+			{
+				out = MK_KEY_RIGHT;
+			}break;
+			case 'D':
+			{
+				out = MK_KEY_LEFT;
+			}break;
+			case '~':
+			{
+				out = MK_KEY_DEL;
+			}break;
+		}
+		
+	}
+	
+	return out;
+}
+
 struct MK_Editor
 {
 	v2i pos;
+	v2i size;
 };
 
 global MK_Editor editor;
@@ -46,17 +99,18 @@ internal void mk_buffer_push(MK_Buffer *buffer, char *c, int len)
 #define mk_buffer_push_reset_cursor(buffer) mk_buffer_push(buffer, "\x1b[H", 3)
 #define mk_buffer_push_hide_cursor(buffer) mk_buffer_push(buffer, "\x1b[?25l", 6)
 #define mk_buffer_push_show_cursor(buffer) mk_buffer_push(buffer, "\x1b[?25h", 6);
+#define mk_buffer_push_clear_screen(buffer) mk_buffer_push(buffer, "\x1b[2J", 4);
 
 internal void mk_buffer_pushf(MK_Buffer *buffer, Arena *arena, const char *fmt, ...)
 {
-  va_list args;
-  va_start(args, fmt);
-  
-  char str[256];
-  u32 len = stbsp_vsnprintf(str, 256, fmt, args);
-  
-  va_end(args);
-  
+	va_list args;
+	va_start(args, fmt);
+	
+	char str[256];
+	u32 len = stbsp_vsnprintf(str, 256, fmt, args);
+	
+	va_end(args);
+	
 	mk_buffer_push(buffer, str, len);
 }
 
@@ -80,9 +134,9 @@ internal void enable_raw_mode()
 	termios raw;
 	tcgetattr(STDIN_FILENO, &raw);
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-  raw.c_oflag &= ~(OPOST);
-  raw.c_cflag |= (CS8);
-  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -107,32 +161,58 @@ int main()
 		MK_Buffer buf = {};
 		buf.base = push_array(&arena, char, Megabytes(1));
 		
+		mk_buffer_push_clear_screen(&buf);
 		mk_buffer_push_hide_cursor(&buf);
 		mk_buffer_push_reset_cursor(&buf);
+		
+		editor.size = get_win_size();
 		
 		if(c == CTRL_KEY('q'))
 		{
 			break;
 		}
-		switch (c) {
-			case 'a':
-      editor.pos.x--;
-      break;
-			case 'd':
-      editor.pos.x++;
-      break;
-			case 'w':
-      editor.pos.y--;
-      break;
-			case 's':
-      editor.pos.y++;
-      break;
+		
+		MK_KEY mv_key = mk_key_from_char(c);
+		
+		switch(mv_key)
+		{
+			case MK_KEY_UP:
+			{
+				if(editor.pos.y > 0)
+				{
+					editor.pos.y --;
+				}
+			}break;
+			case MK_KEY_DOWN:
+			{
+				if(editor.pos.y < editor.size.y - 1)
+				{
+					editor.pos.y ++;
+				}
+			}break;
+			case MK_KEY_RIGHT:
+			{
+				if(editor.pos.x < editor.size.x - 1)
+				{
+					editor.pos.x ++;
+				}
+			}break;
+			case MK_KEY_LEFT:
+			{
+				if(editor.pos.x > 0)
+				{
+					editor.pos.x --;
+				}
+			}break;
+			default:
+			{
+				
+			}
 		}
+		
 		// rows
 		{
-			v2i size = get_win_size();
-			
-			for(i32 i = 0; i < size.y - 1; i ++)
+			for(i32 i = 0; i < editor.size.y - 1; i ++)
 			{
 				mk_buffer_pushf(&buf, &arena, "~\r\n");
 			}
@@ -140,6 +220,8 @@ int main()
 		}
 		
 		mk_buffer_pushf(&buf, &arena, "\x1b[%d;%dH", editor.pos.y + 1, editor.pos.x + 1);
+		
+		mk_buffer_pushf(&buf, &arena, "%c", c);
 		
 		mk_buffer_push_show_cursor(&buf);
 		mk_buffer_submit(&buf);
