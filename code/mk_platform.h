95a1c6a535432a85b5ebc8dce24827c525b37706
oh-facts <as17793@nyu.edu>
2024-07-16 03:52:34 +0530

    not borken yet

--- code/mk_platform.h
+++ code/mk_platform.h
@@ -8,7 +8,7 @@
 #define MK_VERSION_PATCH (0)
 
 #include "stdio.h"
-
+#include "string.h"
 #include <unistd.h>
 #include "base_core.h"
 typedef void *(*os_reserve_fn)(u64 size);
@@ -59,7 +59,16 @@ internal void mk_global_platform_api_init(MK_Platform_api *api)
 enum DEBUG_CYCLE_COUNTER
 {
 	DEBUG_CYCLE_COUNTER_UPDATE_AND_RENDER,
+	DEBUG_CYCLE_COUNTER_RECORD,
+	DEBUG_CYCLE_COUNTER_SUBMIT,
 	DEBUG_CYCLE_COUNTER_COUNT
+};
+
+global char *debug_cycle_to_str[DEBUG_CYCLE_COUNTER_COUNT] = 
+{
+	"update and render",
+	"record",
+	"submit",
 };
 
 struct debug_cycle_counter
@@ -84,6 +93,8 @@ struct MK_Platform
 	MK_Platform_api api;
 	b32 initialized;
 	b32 reloaded;
+	u64 res;
+	u64 cmt;
 	void *memory;
 };
 
